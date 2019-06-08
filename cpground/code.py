adfd4cc5ff7592268a126d97f8f2e8f2a1caa3ec
Jonathan Lipps <jlipps@gmail.com>
2019-06-08 14:28:56 -0700

    add 4th button and sounds

--- cpground/code.py
+++ cpground/code.py
@@ -4,21 +4,42 @@ import audioio
 import board
 
 
-LEDS = [[board.A1, 6], [board.A2, 8], [board.A3, 9]]
-PINS = []
-for idx, spec in enumerate(LEDS):
-    pin = DigitalInOut(spec[0])
-    pin.direction = Direction.INPUT
-    pin.pull = Pull.UP
-    PINS.append({'pin': pin, 'np': spec[1]})
+INPUTS = [
+    {'board': board.A1, 'pixel': 6, 'drum': 'snare-8'},
+    {'board': board.A2, 'pixel': 8, 'drum': 'kick-8'},
+    {'board': board.A5, 'pixel': 1, 'drum': 'hihat-8'},
+    {'board': board.A6, 'pixel': 3, 'drum': 'rimshot'},
+]
 
-cpx.pixels.brightness = 0.1
+
+def play_file(filename):
+    wave_file = open(filename, "rb")
+    with audioio.WaveFile(wave_file) as wave:
+        with audioio.AudioOut(board.A0) as audio:
+            print("playing %s" % filename)
+            audio.play(wave)
+            while audio.playing:
+                pass
+
+
+def init():
+    for input_spec in INPUTS:
+        pin = DigitalInOut(input_spec['board'])
+        pin.direction = Direction.INPUT
+        pin.pull = Pull.UP
+        input_spec['pin'] = pin
+
+    cpx.pixels.brightness = 0.1
+
+
+init()
 
 
 while True:
-    for pin in PINS:
-        if pin['pin'].value:
-            cpx.pixels[pin['np']] = (255, 0, 0)
+    for input_spec in INPUTS:
+        if input_spec['pin'].value:
+            cpx.pixels[input_spec['pixel']] = (255, 0, 0)
         else:
-            cpx.pixels[pin['np']] = (0, 255, 0)
+            cpx.pixels[input_spec['pixel']] = (0, 255, 0)
+            play_file("%s.wav" % input_spec['drum'])
 
